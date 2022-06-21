cd24abf636f53c7bdd517a17d434466e0168d908
DocSJ1980 <docsaj@outlook.com>
2022-06-21 13:57:48 -0700

    4th Commit - express validator implemented

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
-    obj = {
-        a: 'thios',
-        number: 34
+//Create a user using: POST "/api/auth/" Does not require auth
+router.post('/', [
+    body('name', 'Please enter a valid name').isLength({ min: 3 }),
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Please enter a valid password').isLength({ min: 5 }),
+], (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj)
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err=> {console.log(err)
+        res.json({error: 'Please enter a unique value for email', message: err.message})})
+     
 } )
 
 module.exports = router
