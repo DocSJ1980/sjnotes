a89342947eabb8eb9391d59f9c24ee492168825f
DocSJ1980 <docsaj@outlook.com>
2022-06-22 09:19:46 -0700

    5th Commit - 1st ThunderClient collection made

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,24 +3,36 @@ const User = require('../models/User')
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-//Create a user using: POST "/api/auth/" Does not require auth
-router.post('/', [
+//Create a user using: POST "/api/auth/createuser" Does not require auth
+router.post('/createuser', [
     body('name', 'Please enter a valid name').isLength({ min: 3 }),
     body('email', 'Please enter a valid email').isEmail(),
-    body('password', 'Please enter a valid password').isLength({ min: 5 }),
-], (req, res)=>{
+    body('password', 'Password must be atleast 5 characters long.').isLength({ min: 5 }),
+], async (req, res)=>{
+//If there are errors, return bad reqquest aand the rrors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+//Check whether the user with the same email exisits
+try {
+    let user = await User.findOne({email: req.body.email});
+if(user){
+    return res.status(400).json({error: "Sorry a user with this email already exists"})
+}
+
+user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err=> {console.log(err)
-        res.json({error: 'Please enter a unique value for email', message: err.message})})
-     
+    })
+
+res.json(user)
+} 
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error occurred");
+    }
 } )
 
 module.exports = router
