fc790f449da2078799a946777e8f30d3c1c8a6d0
DocSJ1980 <docsaj@outlook.com>
2022-06-25 11:43:52 -0700

    11th Commit - Navbar and react router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<App />}/>
+          <Route exact index element={<Home />} />
+          <Route exact path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
+      <h1>This is sjNotes App</h1>
+    </>
   );
 }
 
