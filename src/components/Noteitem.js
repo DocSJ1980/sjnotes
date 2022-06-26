9c48f6888cd284907bfb15897d0e4322c9f6e141
DocSJ1980 <docsaj@outlook.com>
2022-06-26 13:04:30 -0700

    13th Commit - Errors removed, alerts present, basic alert component created but functionality pending

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -6,8 +6,14 @@ const Noteitem = (props) => {
     <div className='col-md-3'>
         <div className="card my-3">
             <div className="card-body">
-                <h5 className="card-title">{note.title}</h5>
-                <p className="card-text">{note.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Id soluta accusamus, rerum eius totam voluptates voluptatum sint eligendi nihil consequatur quae esse quibusdam, illo explicabo alias in laudantium beatae nam? Dolore unde sequi odit?</p>
+                
+                <div className="d-flex align-items-center">
+                    <h5 className="card-title">{note.title}</h5>
+                    <i className="fa-solid fa-trash mx-2"></i>
+                    <i className="fa-solid fa-pen-to-square mx-2"></i>
+                </div>
+                    <p className="card-text">{note.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Id soluta accusamus, rerum eius totam voluptates voluptatum sint eligendi nihil consequatur quae esse quibusdam, illo explicabo alias in laudantium beatae nam? Dolore unde sequi odit?</p>
+                
             </div>
         </div>
     </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import noteContext from "./noteContext";
 const NoteState = (props) =>{
     const notesInitial = [
         {
-            "_id": "62b85bdf7427053f95a5d9e3",
+            "_id": "62b85bdf7427053f95a5s9e3",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Note",
             "description": "Please wake up early",
@@ -13,7 +13,7 @@ const NoteState = (props) =>{
             "__v": 0
           },
           {
-            "_id": "62b85bfd7427053f95a5d9e5",
+            "_id": "62b15bfd7427053f95a5d9e5",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Program",
             "description": "Coding is my love",
@@ -22,7 +22,7 @@ const NoteState = (props) =>{
             "__v": 0
           },
           {
-            "_id": "62b85bdf7427053f95a5d9e3",
+            "_id": "62b25bdf7427053f95a5d9e3",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Note",
             "description": "Please wake up early",
@@ -31,7 +31,7 @@ const NoteState = (props) =>{
             "__v": 0
           },
           {
-            "_id": "62b85bfd7427053f95a5d9e5",
+            "_id": "62b85bfd7127053f95a5d9e5",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Program",
             "description": "Coding is my love",
@@ -40,7 +40,7 @@ const NoteState = (props) =>{
             "__v": 0
           },
           {
-            "_id": "62b85bdf7427053f95a5d9e3",
+            "_id": "62b85bdf7327053f95a5d9e3",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Note",
             "description": "Please wake up early",
@@ -49,24 +49,7 @@ const NoteState = (props) =>{
             "__v": 0
           },
           {
-            "_id": "62b85bfd7427053f95a5d9e5",
-            "user": "62b35aa7eb7cc2fefae311e2",
-            "title": "My Program",
-            "description": "Coding is my love",
-            "tag": "personal",
-            "date": "2022-06-26T13:15:41.393Z",
-            "__v": 0
-          },{
-            "_id": "62b85bdf7427053f95a5d9e3",
-            "user": "62b35aa7eb7cc2fefae311e2",
-            "title": "My Note",
-            "description": "Please wake up early",
-            "tag": "personal",
-            "date": "2022-06-26T13:15:11.204Z",
-            "__v": 0
-          },
-          {
-            "_id": "62b85bfd7427053f95a5d9e5",
+            "_id": "62b85bfd7427053r95a5d9e5",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Program",
             "description": "Coding is my love",
@@ -75,6 +58,23 @@ const NoteState = (props) =>{
             "__v": 0
           },{
             "_id": "62b85bdf7427053f95a5d9e3",
+            "user": "62b35aa7eb7cc2tefae311e2",
+            "title": "My Note",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2022-06-26T13:15:11.204Z",
+            "__v": 0
+          },
+          {
+            "_id": "62b85bfd7427053f95a5d9q5",
+            "user": "62b35aa7eb7cc2fefae311e2",
+            "title": "My Program",
+            "description": "Coding is my love",
+            "tag": "personal",
+            "date": "2022-06-26T13:15:41.393Z",
+            "__v": 0
+          },{
+            "_id": "62b85bdf7427053f95a5d9w3",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Note",
             "description": "Please wake up early",
@@ -83,7 +83,7 @@ const NoteState = (props) =>{
             "__v": 0
           },
           {
-            "_id": "62b85bfd7427053f95a5d9e5",
+            "_id": "62b85bfd7427053f95p5d9e5",
             "user": "62b35aa7eb7cc2fefae311e2",
             "title": "My Program",
             "description": "Coding is my love",
